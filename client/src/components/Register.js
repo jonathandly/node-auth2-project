e35da942ddfb49d037fe56bb7b8e759bdec5eaa4
Jonathan Daly <dalyj138@gmail.com>
2020-07-26 19:11:07 -0400

    added Login and Register components and created a couple of helper files for authentication.

---
+++ client/src/components/Register.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import api from "../helpers/api";
+
+class Register extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    department: "",
+  };
+
+  submit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { username, password, department } = this.state;
+
+      const result = await api.post("/register", {
+        username,
+        password,
+        department,
+      });
+      localStorage.setItem("token", result.data.token);
+      this.props.history.push("/users");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Register</h1>
+
+        <form onSubmit={this.submit}>
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            onChange={this.handleChange}
+            value={this.state.username}
+          />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={this.handleChange}
+            value={this.state.password}
+          />
+          <input
+            type="text"
+            id="department"
+            placeholder="Department"
+            onChange={this.handleChange}
+            value={this.state.department}
+          />
+          <button type="submit">Register</button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default withRouter(Register);
